88ed9a3d7d85caa10974374128bcc6c97ac9b680
xouabita <xouabita@gmail.com>
2014-04-01 18:32:49 +0200

    Add empty source

---
+++ HW3/GPS/src/main.cpp
@@ -0,0 +1,25 @@
+#ifndef __PROGTEST__
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#endif
+
+class CCoord {
+  public:
+  CCoord (double lat, double lon) {
+    /// Fix Me
+  }
+};
+
+class CGPS {
+  /// Fix me
+};
